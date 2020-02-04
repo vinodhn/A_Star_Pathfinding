670dc4258bb824c34eca23048724d05fff3dd1f1
Vinodh N <vinodhnagarajan@outlook.com>
2020-02-04 05:01:50 -0700

    Finished algorithm

--- AStar.py
+++ AStar.py
@@ -44,3 +44,59 @@ def pathfind(grid, start, end):
         # Move current node from open list to closed list
         openNodesList.pop(currentIndex)
         closedNodesList.append(currentNode)
+
+    # If current node is the end goal
+    if currentNode == endNode:
+        
+        # Create list for nodes in path
+        path = []
+        current = currentNode
+
+        while current is not None;
+            path.append(current.position)
+            current = current.parent
+        
+        # Once processed, return the reversed list of nodes
+        return path[::-1]
+
+    # Check surrounding nodes
+    children = []
+    for pos in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+
+        # Get the position of the node
+        nodePos = (currentNode.position[0] + pos[0], currentNode.position[1] + pos[1])
+
+        # Then make sure it is within the grid
+        if nodePos[0] > (len(grid) - 1) or nodePos[0] < 0 or nodePos[1] > (len(grid[len(grid) -1]) -1) or nodePos[1] < 0:
+            continue
+
+        if grid[nodePos[0]][nodePos[1]] != 0:
+            continue
+
+        # Then create a new node to add to children
+        newChildNode = Node(currentNode, nodePos)
+
+        children.append(newChildNode)
+
+    # Then check the children
+    for child in children:
+
+        # See if the child is closed already
+        for closed in closedNodesList:
+            if child == closed:
+                continue
+                
+        # Calculate the three costs
+        child.gCost = currentNode.gCost + 1
+        child.hCost = ((child.position[0] - endNode.position[0]) ** 2) + ((child.position[1] - endNode.position[1]) ** 2)
+        child.fCost = child.gCost + child.hCost
+
+        # See if child is open already
+        for open in openNodesList:
+            if child == open and child.gCost > open.gCost:
+                continue
+        
+        # Add to open list once evaluated
+        openNodesList.append(child)
+
+
